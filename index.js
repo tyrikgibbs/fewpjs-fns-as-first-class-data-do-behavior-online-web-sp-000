2b3dd7315a7c5b4c0fd97c23358081c4a30520b1
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 23:53:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+
+  
+}
+
 /* Write your implementation of displayMessage() */
+displayMessage()
